bb689c6c9586e97e8346615380a59fa91f224aa3
Grant <gergely.daniel7@gmail.com>
2022-09-27 00:29:02 +0200

    Remove async-await from either

--- src/validators/either.ts
+++ src/validators/either.ts
@@ -3,24 +3,25 @@ import validate from '../validate.js';
 
 const either =
     <V extends Validator<unknown>>(validators: V[]): ValidatorFunction<InferValidator<V>> =>
-    async (value, conf) => {
-        const results = await Promise.allSettled(
+    (value, conf) => {
+        return Promise.allSettled(
             validators.map(
                 (validator) => validate(validator, conf)(value, conf) as InferValidator<V>
             )
-        );
-        const fulfilledPromise = results.find(
-            <T>(result: PromiseSettledResult<T>): result is PromiseFulfilledResult<T> =>
-                result.status === 'fulfilled'
-        );
-        if (fulfilledPromise) {
-            return Success(fulfilledPromise.value);
-        }
-        const rejectedPromises = results.filter(
-            <T>(result: PromiseSettledResult<T>): result is PromiseRejectedResult =>
-                result.status === 'rejected'
-        );
-        throw rejectedPromises.flatMap((result) => result.reason);
+        ).then((results) => {
+            const fulfilledPromise = results.find(
+                <T>(result: PromiseSettledResult<T>): result is PromiseFulfilledResult<T> =>
+                    result.status === 'fulfilled'
+            );
+            if (fulfilledPromise) {
+                return Success(fulfilledPromise.value);
+            }
+            const rejectedPromises = results.filter(
+                <T>(result: PromiseSettledResult<T>): result is PromiseRejectedResult =>
+                    result.status === 'rejected'
+            );
+            throw rejectedPromises.flatMap((result) => result.reason);
+        });
     };
 
 export default either;
