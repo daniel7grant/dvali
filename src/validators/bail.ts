d7c6cfc535298f46e99f953c5660ea1aa9fe1255
Grant <gergely.daniel7@gmail.com>
2022-12-04 01:37:44 +0100

    Fix builds for the rest of the functions

--- src/validators/bail.ts
+++ src/validators/bail.ts
@@ -2,24 +2,34 @@ import {
     SyncValidatorFunction,
     SyncValidatorFunctionList,
     ValidatorConfiguration,
+    ValidatorFunction,
+    ValidatorFunctionList,
     ValidatorState,
 } from '../types.js';
 import validate, { isPromise } from '../validate.js';
 
-const bail = <I, A, O, B>(
+function bail<I, A, O, B>(
     validators: SyncValidatorFunctionList<I, A, O, B>
-): ((val: I, c: ValidatorConfiguration) => O | Promise<O>) => {
+): (val: I, c: ValidatorConfiguration) => O;
+function bail<I, A, O, B>(
+    validators: ValidatorFunctionList<I, A, O, B>
+): (val: I, c: ValidatorConfiguration) => Promise<O>;
+function bail<I, A, O, B>(
+    validators: ValidatorFunctionList<I, A, O, B>
+): (val: I, c: ValidatorConfiguration) => O | Promise<O> {
     if (!Array.isArray(validators)) {
         // If not an array is passed, simply continue validation
         return validate(validators);
     }
     return (value, conf) => {
-        return (validators as SyncValidatorFunction<unknown, unknown>[])
-            .reduce<ValidatorState<unknown> | Promise<ValidatorState<unknown>>>(
-                (previousPromise, validator) => {
-                    return previousPromise.then(({ value, failures }) => {
+        const result = (validators as ValidatorFunction<unknown, unknown>[]).reduce<
+            ValidatorState<unknown> | Promise<ValidatorState<unknown>>
+        >(
+            (previousState, validator) => {
+                if (isPromise(previousState)) {
+                    return previousState.then(({ value, failures }) => {
                         if (failures.length > 0) {
-                            return { value, failures }; // Short-circuit for any error
+                            return previousState;
                         }
                         const result = validator(value, conf);
                         if (isPromise(result)) {
@@ -34,17 +44,50 @@ const bail = <I, A, O, B>(
                                 })
                             );
                         }
-                        return result;
+                        return {
+                            value: typeof result !== 'undefined' ? result : value,
+                            failures: failures,
+                        };
                     });
-                },
-                { value, failures: [] }
-            )
-            .then(({ value, failures }) => {
+                }
+                if (previousState.failures.length > 0) {
+                    return previousState;
+                }
+                const result = validator(value, conf);
+                if (isPromise(result)) {
+                    return result.then(
+                        (newValue) => ({
+                            value: typeof newValue !== 'undefined' ? newValue : value,
+                            failures: previousState.failures,
+                        }),
+                        (failure) => ({
+                            value,
+                            failures: previousState.failures.concat(failure),
+                        })
+                    );
+                }
+                return {
+                    value: typeof result !== 'undefined' ? result : value,
+                    failures: previousState.failures,
+                };
+            },
+            { value, failures: [] } as ValidatorState<unknown>
+        );
+
+        if (isPromise(result)) {
+            return result.then(({ value, failures }) => {
                 if (failures.length > 0) {
                     throw failures;
                 }
                 return value as O;
             });
+        }
+
+        // TODO: handles multiple failures
+        if (result.failures.length > 0) {
+            throw result.failures;
+        }
+        return result.value as O;
     };
-};
+}
 export default bail;
--- src/validators/either.ts
+++ src/validators/either.ts
@@ -1,28 +1,43 @@
 import { InferValidator, Success, Validator, ValidatorConfiguration } from '../types.js';
-import validate from '../validate.js';
+import validate, { isAllNonPromise } from '../validate.js';
 
+function either<V extends Validator<unknown, unknown, unknown, unknown>>(
+    validators: V[]
+): (val: unknown, c: ValidatorConfiguration) => InferValidator<V>;
+function either<V extends Validator<unknown, unknown, unknown, unknown>>(
+    validators: V[]
+): (val: unknown, c: ValidatorConfiguration) => Promise<InferValidator<V>>;
 function either<V extends Validator<unknown, unknown, unknown, unknown>>(
     validators: V[]
 ): (val: unknown, c: ValidatorConfiguration) => InferValidator<V> | Promise<InferValidator<V>> {
     return (value, conf) => {
-        return Promise.allSettled(
-            validators.map(
-                (validator) => validate(validator, conf)(value, conf) as InferValidator<V>
-            )
-        ).then((results) => {
-            const fulfilledPromise = results.find(
-                <T>(result: PromiseSettledResult<T>): result is PromiseFulfilledResult<T> =>
-                    result.status === 'fulfilled'
-            );
-            if (fulfilledPromise) {
-                return Success(fulfilledPromise.value);
+        try {
+            const results = validators.map((validator) => {
+                return validate(validator, conf)(value, conf) as
+                    | InferValidator<V>
+                    | Promise<InferValidator<V>>;
+            });
+
+            if (!isAllNonPromise(results)) {
+                return Promise.allSettled(results).then((results) => {
+                    const fulfilledPromise = results.find(
+                        <T>(result: PromiseSettledResult<T>): result is PromiseFulfilledResult<T> =>
+                            result.status === 'fulfilled'
+                    );
+                    if (fulfilledPromise) {
+                        return fulfilledPromise.value;
+                    }
+                    const rejectedPromises = results.filter(
+                        <T>(result: PromiseSettledResult<T>): result is PromiseRejectedResult =>
+                            result.status === 'rejected'
+                    );
+                    throw rejectedPromises.flatMap((result) => result.reason);
+                });
             }
-            const rejectedPromises = results.filter(
-                <T>(result: PromiseSettledResult<T>): result is PromiseRejectedResult =>
-                    result.status === 'rejected'
-            );
-            throw rejectedPromises.flatMap((result) => result.reason);
-        });
+            return value as InferValidator<V>;
+        } catch (err) {
+            throw err;
+        }
     };
 }
 
